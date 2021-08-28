2775c0fca979aef336fbd76305ed13cfa40281da
shaiful20 <backshaiful18@gmail.com>
2021-08-29 02:43:37 +0600

    Testing

---
+++ js/paragraph.js
@@ -0,0 +1,24 @@
+//=================================================================================//
+//                                search-part start
+//=================================================================================//
+
+var $ = jQuery;
+$(function () {
+    var availableTags = [{
+            value: "Tree Plantation",
+            link: 'https://shaifulkingdom.github.io/acg/pages/paragraph/tree-plantation.html'
+        },
+
+    ];
+    $("#tags").autocomplete({
+        source: availableTags,
+        select: function (event, ui) {
+            console.log(ui.item.link);
+            window.location = ui.item.link;
+        }
+    });
+});
+
+//=================================================================================//
+//                                search-part end
+//=================================================================================//
