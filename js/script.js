c29e629fbe0ebb7d5b19c6cc866ba5b0bad11ca3
shaiful20 <backshaiful18@gmail.com>
2021-09-03 23:08:05 +0600

    new

--- js/script.js
+++ js/script.js
@@ -1,40 +1,14 @@
 //=================================================================================//
 //                                banner-part start
 //=================================================================================//
-$('#banner').slick({
-  dots: true,
-  infinite: true,
-  autoplay: true,
-  speed: 1500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [{
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+$('#banner .container').slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 1500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
 });
-
-
 //=================================================================================//
 //                                banner-part end
 //=================================================================================//
@@ -44,11 +18,56 @@ $('#banner').slick({
 //=================================================================================//
 
 $('.navbar-nav .nav-item').click(function () {
-  $('.navbar-nav .nav-item.active').removeClass('active');
-  $(this).addClass('active');
+    $('.navbar-nav .nav-item.active').removeClass('active');
+    $(this).addClass('active');
 })
 
 //=================================================================================//
 //                                menu-part end
 //=================================================================================//
 
+//=================================================================================//
+//                                book-part start
+//=================================================================================//
+$('#book .book .item').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 1500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplayspeed: 1500,
+    arrows: true,
+    prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+    fade: false,
+    responsive: [{
+            breakpoint: 991.98,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 767.98,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 575.98,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+//=================================================================================//
+//                                book-part end
+//=================================================================================//
